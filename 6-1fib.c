781c77bd825ac112b051f7d0bf0609ea9953efd7
zww <wanwan01012@gmail.com>
2016-09-17 23:02:08 +0800

    fib program

---
+++ 6-1fib.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+//int fib(int n)
+//{
+//	int num1=0;
+//	int num2=1;
+//	int num3=0;
+//	int i=0;
+//
+//	num3=n;
+//	for(i=2;i<=n;i++)
+//	{
+//		num3=num1+num2;
+//		num1=num2;
+//		num2=num3;
+//	}
+//	return num3;
+//}
+//int main()
+//{
+//	int ret=0;
+//	ret=fib(0);
+//	printf("%d\n",ret);
+//	getchar();
+//	return 0;
+//}
