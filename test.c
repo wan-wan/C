c793aecf6fba56e5727accc8b58e0562a3a52ab1
zww <wanwan01012@gmail.com>
2016-09-18 22:31:24 +0800

    rightMove&9*9

---
+++ test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//int main ()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int a = 0;
+//	scanf("%d",&a);
+//    for(j = 1;j <= a;j++)
+//	{
+//		for(i = 1;i<= a;i++)
+//		{
+//			printf("%d*%d=%3d ",i,j,i*j);
+//			if(i == j)
+//			{
+//				printf("\n");
+//				break;
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+ /*Swap(int * x,int * y)
+{
+	int p=0   ;
+	p=*x;
+	*x=*y;
+	*y=p;
+}
+int main()
+{
+	int a=0;
+	int b=0;
+	scanf("%d %d",&a,&b); 
+	Swap(&a,&b);
+	printf("%d %d",a,b);
+	system("pause");
+	return 0;
+}*/
+
+
+
