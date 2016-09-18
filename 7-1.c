eb755c5f4a4b66ae559f94de8c7e90353f025b83
zww <wanwan01012@gmail.com>
2016-09-18 22:22:53 +0800

    caimingci

---
+++ 7-1.c
@@ -0,0 +1,44 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int a=0,b=1,c=0,d=0,e=0;
+//	int i=0;
+//	for(i=0;i<2;i++)
+//	{
+//		if(b=1)
+//		{
+//			;
+//		}
+//		else
+//			a=3;
+//	if(b=2)
+//		{
+//			;
+//		}
+//	else
+//		e=4;
+//	if(c=1)
+//		{
+//			;
+//		}
+//	else
+//		d=3;
+//	if(c=5)
+//		{
+//			;
+//		}
+//	else
+//		d=3;
+//	if(e=4)
+//		{
+//			;
+//		}
+//	else
+//		a=1;
+//	}
+//	printf("a=%d,b=%d,c=%d,d=%d,e=%da",a,b,c,d,e);
+//	system("pause");
+//	return 0;
+//}
