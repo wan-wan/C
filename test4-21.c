958befb82634809a7d1adfbb3ed1db08fd8eb158
wan <wanwan01012@gmail.com>
2016-09-19 21:46:21 +0800

    9*9

---
+++ test4-21.c
@@ -0,0 +1,21 @@
+////¾Å¾Å³Ë·¨±í
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int i=0;
+//	int j=0;
+//	int n=0;
+//	scanf("%d",&n);
+//	for(i=1;i<=n;i++)
+//	{
+//		for(j=1;j<=i;j++)
+//		{
+//			printf("%d*%d=%2d ",j,i,j*i);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//
+//
