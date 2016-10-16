0c2dd481a5740d73d0941b178f8691497fb6dffa
wan <wanwan01012@gmail.com>
2016-10-16 23:18:45 +0800

    wrote a C

---
+++ test10-16.c
@@ -0,0 +1,40 @@
+﻿//#define  _CRT_SECURE_NO_WARNINGS
+//素数
+//#include<stdio.h>
+//#include<math.h>
+//int main ()
+//{int n,k,i,m=0;
+//for(n=101;n<=200;n=n+2)
+//{
+//	k=sqrt(n);
+//for(i=2;i<=k;i++)
+//if(n%i==0)break;
+//if(i>=k+1)
+//{printf("%d ",n);
+//m=m+1;
+//}
+//if (m%10==0)printf("\n");
+//}
+//printf("\n");
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//int a,b;
+//for(a=1;a<=9;a++)
+//for(j=1;b<=a;b++)
+//{
+//	printf("%d*%d=%d",a,b,a*b);
+//if(a!=b){
+//printf("\t");
+//}
+//else
+//if(a=b){
+//printf("\n");
+//}
+//}
+//return 0;
+//}
+//
+//
