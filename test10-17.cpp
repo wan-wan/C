8b5086502bea2a0ecf4a42e3e203c15c71c73c2a
wan <wanwan01012@gmail.com>
2016-10-17 22:58:14 +0800

    wrote a yimyong4

---
+++ test10-17.cpp
@@ -0,0 +1,19 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+using namespace std;
+////值返回  and 引用返回
+//int &fun()
+//{
+//int num=10;
+//return num;//对num做了一份临时拷贝
+//}
+//int main()
+//{
+//int ret=0;
+//ret=fun();
+//printf("hehe\n");
+//cout<<ret<<endl;
+//system("pause");
+//return 0;
+//}
