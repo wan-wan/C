2e39a5336ec3ed5d09026756dc9460bc82b3b1b6
wan <wanwan01012@gmail.com>
2016-10-18 09:54:45 +0800

    wrote a primer1.1

---
+++ primer1-1.cpp
@@ -0,0 +1,75 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+//
+////1.7-1.8
+//int main()
+//{
+//	///*
+//	//*/*  */
+//	//*/
+//	int q=0;
+//	/*cout<<"the sum of"<<q;
+//	    <<"and"<<q;
+//		<<"is"<<q+q<<endl;*/
+//	cout<<q;
+//	cout<<"w"<<"t";
+//	cout<<"q";
+//	cout<<"/*";
+//	cout<<"*/";
+//    //cout<</*"*/"*/;//error
+//	cout<</*"*/" /*"/*"*/;
+//	system ("pause");
+//	return 0;
+//}
+
+
+//1.5
+//int main()
+//{
+//	cout<<"Enter two number "/*<<endl*/;
+//	int b1=0;
+//	int b2=0;
+//	cin>>b1;
+//	cin>>b2;
+//	cout<<"the ji of "<<endl;
+//	cout<<b1<<endl;
+//	cout<<" and "<<endl;
+//	cout<<b2<<endl;
+//	cout<<" is "<<endl;
+//	cout<<b1*b2<<endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//1.4
+//int main()
+//{
+//	cout<<"Enter two number"<<endl;
+//	int  a1=0;
+//	int  a2=0;
+//	cin>>a1>>a2;
+//	cout<<"the sum of "<<a1<<" and "<<a2<<" is "<<a1+a2<<endl;//nultiply    *
+//	system ("pause");
+//	return 0;
+//}
+
+//1.3
+//int main ()
+//{
+//	cout<<"hello world"<<endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//1.1-1.2
+//int main()
+//{
+//	system ("pause");
+//	return 0;//-1
+//}
