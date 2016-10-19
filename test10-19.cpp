bbe7d0bdb84688ccee1518af9d92ecc481dfc474
wan <wanwan01012@gmail.com>
2016-10-19 10:42:05 +0800

    wrote a class1.1

---
+++ test10-19.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+ #include <iostream>
+ using namespace std;
+
+ //class Stu
+ //{
+ //public:
+	// void print()//类内定义成员函数
+	// {
+	//	 cout<<name<<" "<<age<<" "<<sex<<endl;
+	// }
+	// void setData();
+ //private:
+	// char* name;//4
+	// int age;//4
+	// char* sex;//4     all 12
+	// char c;  //
+	// double d;//8  结构体内存对齐
+ //};
+ //void Stu::setData()//类外定义成员函数
+ //{
+	// name="jack";
+	// age=13;
+	// sex="男";
+ //}
+ //int main()
+ //{
+	// Stu stu;//实例化
+	// Stu stu2;
+	// cout<<sizeof(stu)<<endl;
+	// cout<<sizeof(stu2)<<endl;
+	// /*stu.setData();
+	// stu.print();*/
+	// system("pause");
+	// return 0;
+ //}
+
+
+
