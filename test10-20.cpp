bf9d5e184432f0383ea0a06270a7f6dfa84ffea6
wan <wanwan01012@gmail.com>
2016-10-20 23:48:56 +0800

    wrote a primer1.11

---
+++ test10-20.cpp
@@ -0,0 +1,50 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+//1.11 answer
+//int main()
+//{
+//	cout<<"enter two number"<<endl;
+//	int a1,a2;
+//	cin>>a1>>a2;
+//	if(a1>a2)
+//	{
+//		while(a1>=a2)
+//		{
+//			cout<<a1<<" "<<endl;
+//			a1--;
+//		}
+//	}
+//	else
+//		while(a1<=a2)
+//		{
+//			cout<<a1<<" "<<endl;
+//			a1++;
+//		}
+//	system("pause");
+//	return 0;
+//}
+//1.11-2
+//int main()
+//{
+//	cout<<"Enter two number"<<endl;
+//	int a1;
+//	int a2;
+//	cin>>a1>>a2;
+//	if(a1<a2)
+//	{
+//		while(a1<a2-1)
+//		{
+//			cout<<++a1<<endl;
+//		}
+//	}
+//	else
+//	{
+//		while(a2<a1-1)
+//		{
+//			cout<<++a2<<endl;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
